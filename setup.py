5d003acbab1154def3ad26437be1715d1cd10b16
vertigoaway <connormm2@protonmail.com>
2026-03-01 16:33:50 -0500

    Add necessary python package files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="griot",
+    version="0.0.1",
+    author="verti",
+    author_email="verti@piss.industries",
+    description='Text tokenization library',
+    url='https://github.com/vertigoaway/griot',
+    license="MIT",
+    license_files=['LICENSE'],
+    packages=find_packages(),
+    install_requires=[
+        "setuptools==59.6.0",
+    ],
+)
