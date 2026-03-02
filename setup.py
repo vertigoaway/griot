82376feffa275d8cfd7bb69ed662d42561255cb5
vertigoaway <connormm2@protonmail.com>
2026-03-01 19:35:30 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="griot",
-    version="0.0.1",
+    version="0.1.0",
     author="verti",
     author_email="verti@piss.industries",
     description='Text tokenization library',
