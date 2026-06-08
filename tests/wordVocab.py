6c1a3477c9280a75b2d830f00c88635418aea4fe
vertigoaway <connormm2@protonmail.com>
2026-06-07 22:11:18 -0400

    Clarify variables

--- tests/wordVocab.py
+++ tests/wordVocab.py
@@ -6,17 +6,17 @@ class testStrictWordVocab(unittest.TestCase):
 
     def testTokenizerDetokenizer(self):
         tinyDataset = 'apple orange banana tangerine, what is love? baby dont hurt me, baby dont hurt me, no more. this is a cool ass message'
-        vocab = wT.StrictVocab(nulTok=(0,''))
+        vocab = wT.StrictVocab(padTok=(0,''))
 
         vocab.addWords(tinyDataset)
 
-        x = vocab.tokenizeLine(tinyDataset)
-        y = vocab.detokenizeLine(x)
-        z = vocab.tokenizeLine(y)
-        a = vocab.detokenizeLine(z)
-        self.assertEqual(x,z)
-        self.assertEqual(y,a)
-        self.assertEqual(x,[2, 3, 4, 5, 6, 20, 8, 13, 14, 15, 16, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 1])
-        self.assertEqual(y,"apple orange banana tangerine, what is love? baby dont hurt me, baby dont hurt me, no more. this is a cool ass message")
+        tokenizedDataset = vocab.tokenizeLine(tinyDataset)
+        dataset1Pass = vocab.detokenizeLine(tokenizedDataset)
+        tokenized1Pass = vocab.tokenizeLine(dataset1Pass)
+        dataset2Pass = vocab.detokenizeLine(tokenized1Pass)
+        self.assertEqual(tokenizedDataset,tokenized1Pass)
+        self.assertEqual(dataset1Pass,dataset2Pass)
+        self.assertEqual(tokenizedDataset,[2, 3, 4, 5, 6, 20, 8, 13, 14, 15, 16, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 1])
+        self.assertEqual(dataset1Pass,"apple orange banana tangerine, what is love? baby dont hurt me, baby dont hurt me, no more. this is a cool ass message")
 if __name__ == '__main__':
     unittest.main()
