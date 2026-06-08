2fe5030158f4a13a32ab10f65a603b9e588aa3f0
vertigoaway <connormm2@protonmail.com>
2026-06-07 22:03:11 -0400

    Add overloading funcs && Clarify variable names

--- griot/word.py
+++ griot/word.py
@@ -1,35 +1,40 @@
 import pickle
 from typing import Generator
 from typing import cast
-
-
+from typing import overload
 
 class StrictVocab(): #all strings MUST seperate words with the spacer variable
     vocabDict : dict[str,int]
     tokenDict : dict[int,str]
-    nulTok : tuple[int,str]
+    padTok : tuple[int,str]
     eomTok : tuple[int,str]
     spacer : str
     freed : list[int]
-    def __init__(self,nulTok=(0,'[NIL]'),eomTok=(1,'[END]'),spacer=' ') -> None:
-        self.nulTok = nulTok
+    def __init__(self,padTok=(0,'[NIL]'),eomTok=(1,'[END]'),spacer=' ') -> None:
+        self.padTok = padTok
         self.eomTok = eomTok
         self.spacer = spacer
-        self.vocabDict = {nulTok[1]:nulTok[0],eomTok[1]:eomTok[0]}
-        self.tokenDict = {nulTok[0]:nulTok[1],eomTok[0]:eomTok[1]}
+        self.vocabDict = {padTok[1]:padTok[0],eomTok[1]:eomTok[0]}
+        self.tokenDict = {padTok[0]:padTok[1],eomTok[0]:eomTok[1]}
         self.freed  = []
     def save(self,path:str) -> None:
         with open(path,'wb') as file:
-            pickle.dump((self.vocabDict,self.nulTok,self.eomTok,self.freed),file)
+            pickle.dump((self.vocabDict,self.padTok,self.eomTok,self.freed),file)
         return
     def load(self,path:str) -> None:
         with open(path,'rb') as file:
-            self.vocabDict,self.nulTok,self.eomTok,self.freed = pickle.load(file)
+            self.vocabDict,self.padTok,self.eomTok,self.freed = pickle.load(file)
         self.tokenDict = {v: k for k, v in self.vocabDict.items()}
         return
     def __len__(self) -> int:
         """Get the current length of vocab."""
         return len(self.vocabDict)
+
+    @overload
+    def __contains__(self, item: int) -> bool: ...
+    @overload
+    def __contains__(self, item: str) -> bool: ...
+
     def __contains__(self, item : int | str) -> bool:
         """Check if the specified token/index is set
         Args:
@@ -40,10 +45,18 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
             return self.vocabDict.get(item)==str
         else:
             raise TypeError
-    def __delitem__(self, key : int | str) -> None:
+    
+
+    @overload
+    def __delitem__(self, key: int) -> None: ...
+    @overload
+    def __delitem__(self, key: str) -> None: ...
+
+    def __delitem__(self,key : str | int) -> None:
         """Deletes the specified token/index.
         Args:
-            key: The token/index to delete."""
+            key: The token to delete."""
+        
         if type(key) == int:
             y : str = self.tokenDict[key]
             del self.tokenDict[key]
@@ -54,22 +67,28 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
             del self.vocabDict[key]
             del self.tokenDict[x]
             self.freed.append(x)
-        else:
-            raise TypeError
         return
-    def __getitem__(self, key: int | str) -> int | str:
+    
+
+    @overload
+    def __getitem__(self, key: int) -> str: ...
+    @overload
+    def __getitem__(self, key: str) -> int: ...
+
+    def __getitem__(self, key: int | str) -> str | int:
         if type(key) == int:
-            x = self.tokenDict.get(key,self.nulTok[0])
-            if x == None:
-                return self.nulTok[1]
-            return x
+            return self.tokenDict.get(key, self.padTok[1])
         elif type(key) == str:
-            x = self.vocabDict.get(key,self.nulTok[0])
-            if x == None:
-                return self.nulTok[0]
-            return x
+            return self.vocabDict.get(key, self.padTok[0])
         else:
             raise TypeError
+
+    @overload
+    def __setitem__(self, key: int, value: str) -> None: ...
+
+    @overload
+    def __setitem__(self, key: str, value: int) -> None: ...
+
     def __setitem__(self, key: int | str, value: int | str) -> None:
         if type(key) == int and type(value) == str:
             value = value.strip(self.spacer)
@@ -82,15 +101,25 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
         else:
             raise TypeError
         return
+    
     def freeIndices(self) -> Generator[int, None, None]:
         while len(self.freed)>0:
             yield self.freed.pop(0)
         x : int = len(self.tokenDict)
         while True:
-            if self.tokenDict.get(x,self.nulTok[1]) == self.nulTok[1]:
+            if self.tokenDict.get(x,self.padTok[1]) == self.padTok[1]:
                 yield x
             x+=1
+
+    @overload
+    def addWords(self, words: list[str]) -> None: ...
+    @overload
+    def addWords(self, words: str) -> None: ...
+
     def addWords(self,words:list[str] | str) -> None:
+        """Adds all words from a list of strings.
+        Args:
+            words: List of strings with words seperated by spacers."""
         if type(words) == str:
             words = words.split(self.spacer)
         indices = self.freeIndices()
@@ -104,14 +133,14 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
         for word in line.split(self.spacer):
             if len(word)<1:
                 continue
-            out.append(self.vocabDict.get(word,self.nulTok[0]))
+            out.append(self.vocabDict.get(word,self.padTok[0]))
         out.append(self.eomTok[0])
         return out
 
     def detokenizeLine(self,line:list[int]) -> str:
         out = ''
         for index in line:
-            out += self.tokenDict.get(index,self.nulTok[1])+self.spacer
+            out += self.tokenDict.get(index,self.padTok[1])+self.spacer
         return out[:-len(self.eomTok[1]) - 1 - len(self.spacer)] #remove last space
     def tokenizeLines(self,lines:list[str] | str) -> list[list[int]]:
         out = []
@@ -120,6 +149,13 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
         for line in lines:
             out.append(self.tokenizeLine(line))
         return out
+    
+
+    @overload
+    def detokenizeLines(self,lines:list[list[int]]) -> list[str]: ...
+    @overload
+    def detokenizeLines(self,lines:list[int]) -> list[str]: ...
+
     def detokenizeLines(self,lines:list[list[int]] | list[int]) -> list[str]:
         out = []
         line : list[int]
@@ -150,67 +186,86 @@ class StrictVocab(): #all strings MUST seperate words with the spacer variable
 class Vocab(): #no spacers, allows encoding parts of words
     vocabDict : dict[str,int]
     tokenDict : dict[int,str]
-    nulTok : tuple[int,str]
+    padTok : tuple[int,str]
     eomTok : tuple[int,str]
     freed : list[int]
-    def __init__(self,nulTok=(0,'[NIL]'),eomTok=(1,'[END]')) -> None:
-        self.nulTok = nulTok
+    def __init__(self,padTok=(0,'[NIL]'),eomTok=(1,'[END]')) -> None:
+        self.padTok = padTok
         self.eomTok = eomTok
-        self.vocabDict = {nulTok[1]:nulTok[0],eomTok[1]:eomTok[0]}
-        self.tokenDict = {nulTok[0]:nulTok[1],eomTok[0]:eomTok[1]}
+        self.vocabDict = {padTok[1]:padTok[0],eomTok[1]:eomTok[0]}
+        self.tokenDict = {padTok[0]:padTok[1],eomTok[0]:eomTok[1]}
         self.freed  = []
     def save(self,path:str) -> None:
         with open(path,'wb') as file:
-            pickle.dump((self.vocabDict,self.nulTok,self.eomTok,self.freed),file)
+            pickle.dump((self.vocabDict,self.padTok,self.eomTok,self.freed),file)
         return
     def load(self,path:str) -> None:
         with open(path,'rb') as file:
-            self.vocabDict,self.nulTok,self.eomTok,self.freed = pickle.load(file)
+            self.vocabDict,self.padTok,self.eomTok,self.freed = pickle.load(file)
         self.tokenDict = {v: k for k, v in self.vocabDict.items()}
         return
     def __len__(self) -> int:
         """Get the current length of vocab."""
         return len(self.vocabDict)
+    @overload
+    def __contains__(self, item: int) -> bool: ...
+    @overload
+    def __contains__(self, item: str) -> bool: ...
+
     def __contains__(self, item : int | str) -> bool:
         """Check if the specified token/index is set
         Args:
             item: The token/index to check"""
         if type(item) == int:
-            return type(self.tokenDict.get(item,self.nulTok[0]))==int
+            return type(self.tokenDict.get(item))==int
         elif type(item) == str:
-            return self.vocabDict.get(item,self.nulTok[1])==str
+            return self.vocabDict.get(item)==str
         else:
             raise TypeError
-    def __delitem__(self, key : int | str) -> None:
+    
+
+    @overload
+    def __delitem__(self, key: int) -> None: ...
+    @overload
+    def __delitem__(self, key: str) -> None: ...
+
+    def __delitem__(self,key : str | int) -> None:
         """Deletes the specified token/index.
         Args:
-            key: The token/index to delete."""
+            key: The token to delete."""
+        
         if type(key) == int:
-            x = self.tokenDict[key]
+            y : str = self.tokenDict[key]
             del self.tokenDict[key]
-            del self.vocabDict[x]
+            del self.vocabDict[y]
             self.freed.append(key)
         elif type(key) == str:
-            y = self.vocabDict[key]
+            x : int = self.vocabDict[key]
             del self.vocabDict[key]
-            del self.tokenDict[y]
-            self.freed.append(y)
-        else:
-            raise TypeError
+            del self.tokenDict[x]
+            self.freed.append(x)
         return
-    def __getitem__(self, key: int | str) -> int | str:
+    
+
+    @overload
+    def __getitem__(self, key: int) -> str: ...
+    @overload
+    def __getitem__(self, key: str) -> int: ...
+
+    def __getitem__(self, key: int | str) -> str | int:
         if type(key) == int:
-            x = self.tokenDict.get(key,self.nulTok[1])
-            if x == None:
-                return self.nulTok[1]
-            return x
+            return self.tokenDict.get(key, self.padTok[1])
         elif type(key) == str:
-            y = self.vocabDict.get(key,self.nulTok[0])
-            if y == None:
-                return self.nulTok[0]
-            return y
+            return self.vocabDict.get(key, self.padTok[0])
         else:
             raise TypeError
+
+    @overload
+    def __setitem__(self, key: int, value: str) -> None: ...
+
+    @overload
+    def __setitem__(self, key: str, value: int) -> None: ...
+
     def __setitem__(self, key: int | str, value: int | str) -> None:
         if type(key) == int and type(value) == str:
             self.tokenDict[key] = value
@@ -226,7 +281,7 @@ class Vocab(): #no spacers, allows encoding parts of words
             yield self.freed.pop(0)
         x : int = len(self.tokenDict)
         while True:
-            if self.tokenDict.get(x,self.nulTok[1]) == self.nulTok[1]:
+            if self.tokenDict.get(x,self.padTok[1]) == self.padTok[1]:
                 yield x
                 x+=1
     def addWords(self,words:list[str]) -> None:
